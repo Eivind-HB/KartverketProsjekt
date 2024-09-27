29e32bd81454adc59b936ebd8132926c208be5cb
Eivind Hovden Breivik <eivindhb@uia.no>
2024-09-27 13:53:56 +0200

    la til Leaflet mapLayers og kontroller for dette

--- Kartverket/wwwroot/js/site.js
+++ Kartverket/wwwroot/js/site.js
@@ -5,16 +5,30 @@
 
 
 //Lager en kartvariabel med koodrinater for å vise hele Norge, tar i bruk kode fra https://leafletjs.com/
-var map = L.map('map').setView([62.0, 10.0], 5);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+var map = L.map('map').setView([60.14, 10.25], 5);
 
 
+//Lager kart variablene. KAN VÆRE LURT Å VISE TIL HVOR KARTA ER FRA 
+var topographyLayer = L.tileLayer('https://cache.kartverket.no/v1/wmts/1.0.0/topo/default/webmercator/{z}/{y}/{x}.png')
+    grayToneLayer = L.tileLayer('https://cache.kartverket.no/v1/wmts/1.0.0/topograatone/default/webmercator/{z}/{y}/{x}.png'),
+    hikingLayer = L.tileLayer('https://cache.kartverket.no/v1/wmts/1.0.0/toporaster/default/webmercator/{z}/{y}/{x}.png'),
+    seaLayer = L.tileLayer('https://cache.kartverket.no/v1/wmts/1.0.0/sjokartraster/default/webmercator/{z}/{y}/{x}.png');
+
+
+//Navngir Karttypene
+var mapLayers = {
+    "Topografisk kart": topographyLayer,
+    "Gråtone kart": grayToneLayer,
+    "Tur kart": hikingLayer,
+    "Sjø Kart": seaLayer
+};
+
+//Setter topographyLayer som basis kart
+topographyLayer.addTo(map);
+//Lager leaflet controller som tar i bruk mapLayers
+L.control.layers(mapLayers).addTo(map);
 
 //En funksjon som viser koodrinatene av der man klikker
-
 var popup = L.popup();
 
 function onMapClick(e) {
