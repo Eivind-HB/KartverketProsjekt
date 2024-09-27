3e6f019f64a77e3cf3c2872c25a63fcf520f97fd
NiriLorentzen <144032550+NiriLorentzen@users.noreply.github.com>
2024-09-27 12:14:10 +0200

    Leaflet implementasjon, førsteutkast (#20)
    * La til Leaflet JS og til views
    
    * Fikset på z-position til leafleten, slik at den ligger bak navbar
    
    * slettet en fil som ble nedlastet med uhell...
    
    ---------
    
    Co-authored-by: EivindHB <eivind.h.breivik@gmail.com>

--- Kartverket/wwwroot/js/site.js
+++ Kartverket/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+//Lager en kartvariabel med koodrinater for å vise hele Norge, tar i bruk kode fra https://leafletjs.com/
+var map = L.map('map').setView([62.0, 10.0], 5);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+
+
+//En funksjon som viser koodrinatene av der man klikker
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("Du trykket på: " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
