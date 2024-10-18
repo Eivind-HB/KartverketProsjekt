9c3bbed43b5a8193274ccf00b649793863852542
Eivind-HB <165715590+Eivind-HB@users.noreply.github.com>
2024-10-18 12:36:10 +0200

    La til Kommuneinfo API (#49)
    * La til kommuneInfo Api, og tilhørende view, models, og Services
    
    * Endret slik at KommuneAPI vises på index uten behov for ny view
    
    * Fikset kommentarer

--- Kartverket/Models/AreaChange.cs
+++ Kartverket/Models/AreaChange.cs
@@ -2,8 +2,8 @@
 {
     public class AreaChange
     { 
-        public string Id { get; set; }
-        public string GeoJson { get; set; }
-        public string Description { get; set; }
+        public string? Id { get; set; }
+        public string? GeoJson { get; set; }
+        public string? Description { get; set; }
     }
 }
---
+++ Kartverket/Services/IKommuneInfoApiService.cs
@@ -0,0 +1,9 @@
+﻿using Kartverket.API_Models;
+
+namespace Kartverket.Services
+{
+    public interface IKommuneInfoApiService
+    {
+        Task<KommuneInfo> GetKommuneInfoAsync(string kommuneNr);
+    }
+}
