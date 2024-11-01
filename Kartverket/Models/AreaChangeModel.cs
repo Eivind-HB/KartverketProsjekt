2336372a0af78fcf87ae3973daca4b53c6d4e4fe
EivindHB <eivind.h.breivik@gmail.com>
2024-11-01 02:09:24 +0100

    Fikset merge problemer

--- Kartverket/Models/AreaChangeModel.cs
+++ Kartverket/Models/AreaChangeModel.cs
@@ -4,14 +4,16 @@
     { 
         
         public string? IssueId { get; set; }
+        public string? IssueType { get; set; }
+        public DateTime IssueDate { get; set; }
         public string? GeoJson { get; set; }
         public string? Description { get; set; }
         public string? Kommunenavn { get; set; }
         public string? Kommunenummer { get; set; }
         public string? Fylkesnavn { get; set; }
         public string? Fylkesnummer { get; set; }
-        public string? IssueType { get; set; }
-        public DateTime IssueDate { get; set; }
+        public string? Username { get; set; }
+        
 
     }
 }
