f3237af558cdd20d43260c13256787c2cc53d5fe
NicolayLT <nicolayl@uia.no>
2024-11-04 15:21:51 +0100

    Dropdown Meny/Log in

--- Kartverket/Controllers/HomeController.cs
+++ Kartverket/Controllers/HomeController.cs
@@ -41,16 +41,15 @@ namespace Kartverket.Controllers
             return View();
         }
 
-        [HttpGet]
-        public ViewResult RegistrationForm()
+        public IActionResult LogInForm()
         {
             return View();
         }
 
-        [HttpPost]
-        public ViewResult RegistrationForm(UserData userData)
+        [HttpGet]
+        public ViewResult RegistrationForm()
         {
-            return View("UDOverview", userData);
+            return View();
         }
 
         [HttpGet]
