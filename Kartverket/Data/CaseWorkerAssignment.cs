8f53cd561a291e098dc2e348eb0094ead1fcea7e
NiriLorentzen <144032550+NiriLorentzen@users.noreply.github.com>
2024-11-20 16:21:06 +0100

    Case worker assignment (#110)
    * fikset seeding av KartverketEmployees
    
    * la til CaseWorkerAssignment og seeder inn adminprofilen i CaseWorker tabellen

--- Kartverket/Data/CaseWorkerAssignment.cs
+++ Kartverket/Data/CaseWorkerAssignment.cs
@@ -1,14 +1,15 @@
 ﻿using MySqlConnector;
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.Reflection.Metadata;
 
 namespace Kartverket.Data
 {
-    public class CaseWorker
+    public class CaseWorkerAssignment
     {
         [Key]
+        public int CaseNo { get; set; }
         public int CaseWorkerID { get; set; }
-        public int KartverketEmployee_EmployeeID { get; set; }
-        public string Password { get; set; }
+        public decimal PaidHours { get; set; }
     }
 }
