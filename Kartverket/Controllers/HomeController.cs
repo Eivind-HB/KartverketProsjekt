9d79f45ec45eda4a2b590ca39f0566d8e8f30913
NiriLorentzen <144032550+NiriLorentzen@users.noreply.github.com>
2024-11-22 13:35:19 +0100

    la til unittest for csv filene og begynte på unit test for register area change, la til litt geojson og kommuneinfo inndatasjekk og feilmelding i areachange cshtml-en (#131)

--- Kartverket/Controllers/HomeController.cs
+++ Kartverket/Controllers/HomeController.cs
@@ -131,6 +131,23 @@ namespace Kartverket.Controllers
                 }
             }
 
+
+            //Checks that the geojson in areamodel is not null/empty
+            if (string.IsNullOrEmpty(areaModel.GeoJson))
+            {
+                // Store error message in ViewBag
+                ViewBag.ErrorMessage = "Kartet må være markert!";
+                return View("RoadCorrection"); // Return to the same view with the model
+            }
+
+            //Checks that the description in areamodel is not null/empty
+            if (string.IsNullOrEmpty(areaModel.Kommunenavn))
+            {
+                // Store error message in ViewBag
+                ViewBag.ErrorMessage = "Kommune er ikke regisrert! Prøv å trykk en ekstra gang på kartet etter du har markert det";
+                return View("RoadCorrection"); // Return to the same view with the model
+            }
+
             var newChange = new AreaChange
             {
                 IssueId = Guid.NewGuid().ToString(),
@@ -195,9 +212,9 @@ namespace Kartverket.Controllers
             int CaseNoNumber = rnd.Next(100000, 999999);
 
 
-            TempData["OpprettetSaksnr"] = CaseNoNumber;
-            ViewBag.ViewModel = TempData["OpprettetSaksnr"];
-            TempData.Keep("OpprettetSaksnr");
+            //TempData["OpprettetSaksnr"] = CaseNoNumber;
+            //ViewBag.ViewModel = TempData["OpprettetSaksnr"];
+            //TempData.Keep("OpprettetSaksnr");
 
             //var newlycreated = true;
 
