9cb201ad1b452ed4284c80d12c986899bbf7776d
NiriLorentzen <niri.lorentzen@gmail.com>
2024-11-22 15:08:04 +0100

    fikset den siste unit testen for registerareachange

--- UnitTests/CsvTest.cs
+++ UnitTests/CsvTest.cs
@@ -12,6 +12,7 @@ namespace Kartverket.Test
 {
     public class CsvTests
     {
+
         [Fact]
         public void CaseWorkerTestCsvFileReading()
         {
@@ -25,7 +26,7 @@ namespace Kartverket.Test
 
             // Assert
             Assert.NotNull(records);
-            Assert.Single(records); // Assuming the CSV file has 1 record
+            Assert.Single(records); // Checking that the CSV file has 1 record
             Assert.Equal(1, records.First().CaseWorkerID);
             Assert.Equal(1, records.First().KartverketEmployee_EmployeeID);
         }
@@ -43,7 +44,7 @@ namespace Kartverket.Test
 
             // Assert
             Assert.NotNull(records);
-            Assert.Equal(15, records.Count()); // Assuming the CSV file has 3 records
+            Assert.Equal(15, records.Count()); // Checking that the CSV file has 15 records
             Assert.Equal(03, records.First().FylkesNameID);
             Assert.Equal("Oslo", records.First().FylkesName);
         }
@@ -61,7 +62,7 @@ namespace Kartverket.Test
 
             // Assert
             Assert.NotNull(records);
-            Assert.Equal(357, records.Count()); // Assuming the CSV file has 3 records
+            Assert.Equal(357, records.Count()); // Checking that the CSV file has 357 records
             Assert.Equal(5636, records.First().KommuneInfoID);
             Assert.Equal("Unjárga - Nesseby", records.First().KommuneName);
         }
@@ -79,7 +80,7 @@ namespace Kartverket.Test
 
             // Assert
             Assert.NotNull(records);
-            Assert.Equal(5, records.Count()); // Assuming the CSV file has 3 records
+            Assert.Equal(5, records.Count()); // Checking that the CSV file has 5 records
             Assert.Equal(1, records.First().StatusNo);
             Assert.Equal("Sendt", records.First().StatusName);
         }
@@ -97,14 +98,33 @@ namespace Kartverket.Test
 
             // Assert
             Assert.NotNull(records);
-            Assert.Equal(21, records.Count()); // Assuming the CSV file has 3 records
+            Assert.Equal(21, records.Count()); // Checking that the CSV file has 21 records
             Assert.Equal(1, records.First().EmployeeID);
             Assert.Equal(00000000, records.First().PhoneNo);
-            Assert.Equal("admin@kartverket.no", records.First().Mail);
-            Assert.Equal("Admin", records.First().Title);
+            Assert.Equal("'admin@kartverket.no'", records.First().Mail);
+            Assert.Equal("'Admin'", records.First().Title);
             Assert.Equal(0, records.First().Wage);
-            Assert.Equal("Admin", records.First().Firstname);
-            Assert.Equal("Adminsen", records.First().Lastname);
+            Assert.Equal("'Admin'", records.First().Firstname);
+            Assert.Equal("'Adminsen'", records.First().Lastname);
+
+        }
+
+        [Fact]
+        public void IssueTestCsvFileReading()
+        {
+            // Arrange
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            string projectDirectory = Directory.GetParent(baseDirectory).Parent.Parent.Parent.Parent.FullName;
+            string csvFilePath = Path.Combine(projectDirectory, "Kartverket", "Data", "issue.csv");
+
+            // Act
+            var records = ReadCsvFile<IssueTestRecord>(csvFilePath);
+
+            // Assert
+            Assert.NotNull(records);
+            Assert.Equal(4, records.Count()); // Checking that the CSV file has 4 records
+            Assert.Equal(1, records.First().issueNo);
+            Assert.Equal("Adresse/Tomt", records.First().IssueType);
 
         }
 
@@ -118,29 +138,32 @@ namespace Kartverket.Test
         }
     }
 
+    //Class that reflects the csv file information the test method is fetching from
     public class CaseWorkerTestRecord
     {
         public int CaseWorkerID { get; set; }
         public int KartverketEmployee_EmployeeID { get; set; }
+        // public string Password { get; set; } -- chose not to have this due to privacy and necessity
     }
+    //Class that reflects the csv file information the test method is fetching from
     public class FylkeInfoTestRecord
     {
         public int FylkesNameID { get; set; }
         public string FylkesName { get; set; }
     }
-
+    //Class that reflects the csv file information the test method is fetching from
     public class KommmuneInfoTestRecord
     {
         public int KommuneInfoID { get; set; }
         public string KommuneName { get; set; }
     }
-
+    //Class that reflects the csv file information the test method is fetching from
     public class StatusTestRecord
     {
         public int StatusNo { get; set; }
         public string StatusName { get; set; }
     }
-
+    //Class that reflects the csv file information the test method is fetching from
     public class KartverketEmployeeTestRecord
     {
         public int EmployeeID { get; set; }
@@ -150,5 +173,11 @@ namespace Kartverket.Test
         public int Wage { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
+    }
+    //Class that reflects the csv file information the test method is fetching from
+    public class IssueTestRecord
+    {
+        public int issueNo { get; set; }
+        public string IssueType { get; set; }
     }
 }
