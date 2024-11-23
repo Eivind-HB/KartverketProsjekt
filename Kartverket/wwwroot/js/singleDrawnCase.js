e66cf9cbdad4267b26d737635a67b489e672f997
Eivind Hovden Breivik <eivindhb@uia.no>
2024-11-23 13:45:55 +0100

    Implementerte drawnCase i hasProfileCaseOverview

--- Kartverket/wwwroot/js/singleDrawnCase.js
+++ Kartverket/wwwroot/js/singleDrawnCase.js
@@ -1,13 +1,11 @@
-﻿
-/* 
+﻿/* 
   This function is to display a single DrawnChange on a leafletmap. 
   Needs to be given valid geoJsonData a caseNumber to display
 */
-
-function singleDrawnChange(geoJsonData, CaseNo) { 
+function singleDrawnChange(geoJsonData, caseNo) { 
 
     console.log("GeoJSON Data:", geoJsonData);
-    console.log("CaseNo", caseNumber);
+    console.log("CaseNo", caseNo);
     // Makes sure om GeoJSON-data is valid
     if (geoJsonData) {
         // Adds the GeoJson data to the map
@@ -17,7 +15,7 @@ function singleDrawnChange(geoJsonData, CaseNo) {
             }, //Adds casenumber as popup on click
             onEachFeature: function (feature, layer) {
 
-                var popupContent = `Saksnummer: ${caseNumber}`;
+                var popupContent = `Saksnummer: ${caseNo}`;
 
                 layer.bindPopup(popupContent);
             }
