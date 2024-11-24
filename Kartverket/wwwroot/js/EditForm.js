f4514da347e4c8978ad6912cf66b72ae8c5507f5
Eivind Hovden Breivik <eivindhb@uia.no>
2024-11-24 14:47:17 +0100

    Opprettet felles EditForm script

---
+++ Kartverket/wwwroot/js/EditForm.js
@@ -0,0 +1,9 @@
+﻿// Toggle the edit form visibility
+function toggleEditForm(caseNo) {
+    const form = document.getElementById(`edit-form-${caseNo}`);
+    const editButtons = document.getElementById(`edit-buttons-${caseNo}`);
+    const isHidden = form.style.display === "none";
+
+    form.style.display = isHidden ? "block" : "none";
+    editButtons.style.display = isHidden ? "flex" : "none";
+}
