0e7dbdd2e45beb95c624bc317f93ffdf0231eaca
Eivind Hovden Breivik <165715590+Eivind-HB@users.noreply.github.com>
2024-11-24 15:32:14 +0100

    Partial view til Image Modal og Modulariserte scripts (#158)
    * La til _ImageModal partial view
    
    * Opprettet felles EditForm script
    
    * Oppretter accordion.js for å redusere code duplication
    
    * La til highlightNavItem i site.js

---
+++ Kartverket/wwwroot/js/Accordion.js
@@ -0,0 +1,26 @@
+﻿// Function to initialize accordions in views
+// containerSelector: A CSS selector for the container holding the accordion elements
+// map (optional): if a map is initialized in the container it makes sure it fits with Invalidate Size
+function initializeAccordion(containerSelector, map = null) {
+    // Select all accordion headers within the specified container
+    document.querySelectorAll(`${containerSelector} .accordion-header`).forEach(button => {
+        button.addEventListener('click', () => {
+            const accordionContent = button.nextElementSibling;
+
+            // Toggle the 'active' class on the button
+            button.classList.toggle('active');
+
+            // Toggle display style of the accordion content
+            if (accordionContent.style.display === 'block') {
+                accordionContent.style.display = 'none';
+            } else {
+                accordionContent.style.display = 'block';
+            }
+
+            // If a map object is provided, invalidate its size
+            if (map) {
+                map.invalidateSize();
+            }
+        });
+    });
+}
